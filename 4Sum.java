7de1bf39906b43d2f0bc24328ca4a80a6f9605b3
LinDu <lindu0515@gmail.com>
2015-07-27 23:10:32 -0400

    Three solutions

---
+++ 4Sum.java
@@ -0,0 +1,60 @@
+/*
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+    A solution set is:
+    (-1,  0, 0, 1)
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+ */
+
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int start = j + 1;
+                int end = nums.length - 1;
+                while (start < end) {
+                    int sum = nums[start] + nums[end] + nums[i] + nums[j];
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[start]);
+                        temp.add(nums[end]);
+                        res.add(temp);
+                        start++;
+                        end--;
+                        while (start < end && nums[start] == nums[start - 1]) { // to skip duplicates
+						    start++;
+					    }
+					    while (start < end && nums[end] == nums[end + 1]) { // 		// to skip duplicates
+						    end--;
+					    }
+                    } else if (sum > target) {
+                        end--;
+                    } else {
+                        start++;
+                    }
+                }
+            } 
+        }
+        return res;
+    }
+}
